7e0949d15b27c1bfa1a0ff5b670d9446a98e976e
elky84 <elky84@gmail.com>
2023-08-10 18:16:47 +0900

    board module, service, controller 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BoardsModule } from './boards/boards.module';
+import { BoardsService } from './boards/boards.service';
 
 @Module({
-  imports: [],
+  imports: [BoardsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, BoardsService],
 })
 export class AppModule {}
