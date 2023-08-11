ae0dd57f0d6fefa878303bf91d167194ff40b56c
elky84 <elky84@gmail.com>
2023-08-11 15:26:47 +0900

    예외 처리 보강

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Board, BoardStatus } from './boards.model';
 import {v1 as uuid } from 'uuid'
 import { CreateBoardDto } from './boards.dto';
@@ -24,11 +24,16 @@ export class BoardsService {
     }
 
     getBoardById(id: string) : Board {
-        return this.boards.find((board) => board.id === id)
+        const found = this.boards.find((board) => board.id === id);
+        if(!found){
+            throw new NotFoundException("can't find any id : " + id);
+        }
+        return found;
     }
 
     deleteBoard(id : string){
-        this.boards = this.boards.filter( (board) => board.id !== id)
+        const found = this.getBoardById(id);
+        this.boards = this.boards.filter( (board) => board.id !== found.id)
     }
 
     updateBoardStatus(id : string, status : BoardStatus) : Board{
